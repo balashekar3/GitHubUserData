65b2c783021bf5e8b5352f67a5a102802ff9f559
balashekarcomx <59250547+balashekarcomx@users.noreply.github.com>
2020-02-05 10:56:56 +0530

    Initial Commit

---
+++ GitHubUserData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubUserData
+//
+//  Created by apple on 05/02/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
